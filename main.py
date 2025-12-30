167115fb127e0ddb97c250d6f2b2d9c52a4ee3f3
wojciech-baraniak05 <wbaranniakk@gmail.com>
2025-12-30 11:53:39 +0100

    wszystko gotowe przez billingiem

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 # from transformers import AutoModelForCausalLM, AutoTokenizer
 import numpy as np
-from transformers import pipeline
+# from transformers import pipeline
+from src.lupa.Model import DataModel
+import pandas as pd
+
 
 import pandas as pd
 def main():
     print("wykonano importy")
-    pipe_mod = pipeline("sentiment-analysis")
-    res = pipe_mod("dupsko 1234")
-    print(res)
+    df = pd.read_csv("prompts2.csv", sep=";")
+    X, y = df['Prompt'].values, df['Flag'].values
+    print(type(X))
+    data_model = DataModel(X, y)
+    print(data_model)
+
+
 
 
 if __name__ == "__main__":
